557720d150bee8b96ab5128c028638b8497bb7fc
Byeongmin <minkbm1031@naver.com>
2025-08-02 14:03:42 +0900

    Feat : use pytest and image conversion

---
+++ test_3Dpoint.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('uzumaki_naruto.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+depth_map = cv2.applyColorMap(gray, cv2.COLORMAP_JET)
+
+h, w = depth_map.shape[:2]
+X, Y = np.meshgrid(np.arange(w), np.arange(h))
+Z = gray.astype(np.float32)
+
+points_3d = np.dstack((X, Y, Z))
+
+cv2.imshow('Depth Map', depth_map)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ test_Depthmap.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('uzumaki_naruto.jpg')
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+depth_map = cv2.applyColorMap(gray, cv2.COLORMAP_JET)
+
+cv2.imshow('Original image', image)
+cv2.imshow('Depth Map', depth_map)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
